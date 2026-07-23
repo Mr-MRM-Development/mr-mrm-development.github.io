e9aacdf616664e87bbd443c953e3308f7f8e3a22
Mr MRM Development <mubarokrizqy101@gmail.com>
2026-07-23 21:15:03 +0700

    2026 07 23 21 14

---
+++ js/cover.js
@@ -0,0 +1,11 @@
+const coverPP = document.getElementById("coverPP");
+
+function rotateCoverPP() {
+    if ((coverPP.src).includes("assets/img/photo-profile.png")) {
+        coverPP.src              = "assets/img/bocchi.png"
+    } else {
+        coverPP.src              = "assets/img/photo-profile.png"
+    }
+}
+
+window.rotateCoverPP = rotateCoverPP;
