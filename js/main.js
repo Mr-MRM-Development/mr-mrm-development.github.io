f4989370c2fe84110937ad7a78a7a9a0695880a1
Mr MRM Development <mubarokrizqy101@gmail.com>
2026-07-24 21:31:19 +0700

    2026 07 24 21 07

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 import "./cover.js";
+
+// ============================================
+// LIQUID NOISE ANIMATION
+// ============================================
+
+let t = 0;
+const liquidNoise = document.getElementById("liquid-noise");
+
+function liquidAnimate() {
+    t += 0.01;
+
+    const x = 0.02 + Math.sin(t) * 0.01;
+    const y = 0.03 + Math.cos(t) * 0.01;
+
+    liquidNoise.setAttribute("baseFrequency", `${x}, ${y}`);
+
+    requestAnimationFrame(liquidAnimate);
+}
+
+liquidAnimate();
